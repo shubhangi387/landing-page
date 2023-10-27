93d0eb17150fc6557d1a5cbbf28ea4cf7ee416b0
Shubhangi Mahadev Metakari <134784864+shubhangi387@users.noreply.github.com>
2023-10-27 04:43:14 -0500

    Update login.js

--- frontEnd/src/login.js
+++ frontEnd/src/login.js
@@ -76,7 +76,7 @@ export default function Login1() {
           if((rout=== 'register' && signData.username!=null && signData.password!=null && signData.name!=null)
           || ((rout=== 'login' && formData.username!=null && formData.password!=null))
           ){
-          fetch(`http://localhost:500/${rout}`,{
+          fetch(`https://sodd-dash-board.vercel.app/${rout}`,{
           method:"POST",
           headers:{
           'Content-Type':'application/json'
